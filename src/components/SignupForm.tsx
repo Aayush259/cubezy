30bc90be3eee3da6159eb0e6155272a20c6ab18e
Aayush <aayushk4980@gmail.com>
2024-11-16 22:43:13 +0530

    Created Signup form component

---
+++ src/components/SignupForm.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { useState } from "react";
+
+export default function SignupForm() {
+
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const [error, setError] = useState<boolean>(false);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+
+        if (!formData.name || !formData.email || !formData.password) {
+            setError(true);
+            setIsSubmitting(false);
+            return;
+        }
+    }
+
+    return (
+        <form className="w-full flex flex-col gap-6" onSubmit={handleSubmit}>
+            <label htmlFor="name" className="flex flex-col gap-2 text-lg">
+                <p className="flex items-center justify-between">
+                    <span>Username:</span>
+                    {
+                        error && !formData.name && <span className="text-sm text-red-500 font-semibold">Required*</span>
+                    }
+                </p>
+                <input
+                    type="text"
+                    id="name"
+                    placeholder="Enter your name"
+                    className="w-full px-4 py-2 bg-transparent border border-white/80 rounded-lg"
+                    value={formData.name}
+                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                />
+            </label>
+
+            <label htmlFor="email" className="flex flex-col gap-2 text-lg">
+            <p className="flex items-center justify-between">
+                    <span>Email:</span>
+                    {
+                        error && !formData.email && <span className="text-sm text-red-500 font-semibold">Required*</span>
+                    }
+                </p>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    className="w-full px-4 py-2 bg-transparent border border-white/80 rounded-lg"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+            </label>
+
+            <label htmlFor="name" className="flex flex-col gap-2 text-lg">
+            <p className="flex items-center justify-between">
+                    <span>Password:</span>
+                    {
+                        error && !formData.password && <span className="text-sm text-red-500 font-semibold">Required*</span>
+                    }
+                </p>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Enter your password"
+                    className="w-full px-4 py-2 bg-transparent border border-white/80 rounded-lg"
+                    value={formData.password}
+                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+            </label>
+
+            <button type="submit" className={`px-4 py-2 my-2 w-full bg-orange-700 rounded-lg mx-auto ${isSubmitting ? "opacity-50" : "opacity-100"}`}>
+                Sign Up
+            </button>
+        </form>
+    );
+}
