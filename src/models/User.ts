638f6e9ba10a4a77582e3c085068a298267b7916
Aayush <aayushk4980@gmail.com>
2024-11-25 22:47:15 +0530

    Update User model to store connections array

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,6 +5,7 @@ interface IUser extends Document {
     name: string;
     email: string;
     password: string;
+    connections: {_id: mongoose.Types.ObjectId; name: string}[];
     comparePassword(password: string): Promise<boolean>;
 };
 
@@ -12,6 +13,12 @@ const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    connections: [
+        {
+            _id: { type: mongoose.Types.ObjectId, ref: "User" },
+            name: { type: String, required: true },
+        },
+    ],
 });
 
 // Hash password before saving.
