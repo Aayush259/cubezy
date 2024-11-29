f0d66d7db2933fb5512677d9ce695111a1d5e74e
Aayush <aayushk4980@gmail.com>
2024-11-29 22:04:22 +0530

    Updated Chat model

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -4,16 +4,18 @@ interface IMessage extends Document {
     senderId: mongoose.Types.ObjectId;
     message: string;
     sentAt: Date;
+    isRead: boolean;
 }
 
 const messageSchema = new mongoose.Schema({
     senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     message: { type: String, required: true },
     sentAt: { type: Date, default: Date.now },
+    isRead: { type: Boolean, default: false },
 });
 
 const createMessageModel = (chatId: string): Model<IMessage> => {
-    const collectionName = `chat_${chatId}`;
+    const collectionName = `${chatId}`;
     if (mongoose.models[collectionName]) {
         return mongoose.models[collectionName] as Model<IMessage>;
     }
