218adf2aee2082c0bd4351cdc2b7184147f1e53e
Aayush <aayushk4980@gmail.com>
2024-12-05 20:17:53 +0530

    Added last message below connection's name + small features

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,13 +5,14 @@ import { RootState } from "../store/store";
 import { IoIosArrowForward } from "react-icons/io";
 import { useIdBarContext } from "../contexts/IdBarContext";
 import { useChatContext } from "../contexts/ChatContext";
+import { formatDate } from "../funcs/funcs";
 
 export default function Sidebar() {
 
     const { user } = useSelector((state: RootState) => state.user);
 
     const { setIdBarOpen } = useIdBarContext();
-    const { receiverId, setReceiverId } = useChatContext();
+    const { receiverId, setReceiverId, lastMessages } = useChatContext();
 
     const [greeting, setGreeting] = useState<string>("");   // Greeting message based on the current time.
 
@@ -58,17 +59,46 @@ export default function Sidebar() {
 
             <div className="h-full pb-28 overflow-y-auto">
                 {
-                    user?.connections.map(connection => (
-                        <button
-                            key={connection._id}
-                            className={`w-full px-6 py-4 flex items-center justify-between hover:bg-gray-900 duration-300 border-b border-gray-800 text-xl ${connection._id === receiverId ? "bg-black" : "bg-transparent"}`}
-                            onClick={() => setReceiverId(connection._id)}
-                        >
-                            {connection.name}
-                            <IoIosArrowForward size={20} />
-                        </button>
-                    ))
-                }
+                    user?.connections.map(connection => {
+                        const lastMessage = lastMessages.find(message => message.chatId === connection.chatId)?.lastMessage;
+
+                        return (
+                            <button
+                                key={connection._id}
+                                className={`w-full px-6 py-4 flex items-center justify-between hover:bg-gray-900 duration-300 border-b border-gray-800 text-xl ${connection._id === receiverId ? "bg-black" : "bg-transparent"}`}
+                                onClick={() => setReceiverId(connection._id)}
+                            >
+                                <span className="flex items-center gap-4 flex-grow pr-7">
+                                    <span className="flex items-center justify-center h-[50px] w-[50px] bg-blue-700 text-white text-2xl rounded-full overflow-hidden">
+                                        {connection.name[0]}
+                                    </span>
+                                    <span className="flex flex-col flex-grow justify-between items-start gap-[3px]">
+                                        <span className="block">{connection.name}</span>
+                                        <span className={`text-sm flex items-center justify-between w-full whitespace-nowrap ${lastMessage && !lastMessage.isRead && lastMessage.senderId !== user._id ? "font-bold" : "opacity-70"}`}>
+                                            <span className="block max-w-[200px] overflow-ellipsis overflow-hidden">
+                                                {lastMessage?.message}
+                                            </span>
+
+                                            {
+                                                lastMessage?.sentAt && <span className="flex-grow text-right text-xs">
+                                                    {formatDate(lastMessage.sentAt)}
+                                                </span>
+                                            }
+                                        </span>
+                                    </span>
+                                </span>
+
+                                <span className="flex items-center gap-2">
+                                    {
+                                        lastMessage && !lastMessage.isRead && lastMessage.senderId !== user._id && (
+                                            <span className="block h-4 w-4 rounded-full bg-blue-500" />
+                                        )
+                                    }
+                                    <IoIosArrowForward size={20} />
+                                </span>
+                            </button>
+                        )
+                    })}
 
                 {
                     user?.connections.length === 0 && (
