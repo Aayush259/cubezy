4cea5f86b104430d5d09b095d6cbf64599fec330
Aayush <aayushk4980@gmail.com>
2024-12-06 23:34:08 +0530

    Added ProfileContextProvider in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,33 @@ import { IdBarContextProvider } from "@/src/contexts/IdBarContext";
 import { ChatContextProvider } from "@/src/contexts/ChatContext";
 import ChatWindow from "@/src/components/ChatWindow";
 import Image from "next/image";
+import Profile from "@/src/components/Profile";
+import { ProfileContextProvider } from "@/src/contexts/ProfileContext";
 
 export default function Home() {
 
   return (
     <div className="h-screen flex flex-row overflow-hidden font-[family-name:var(--font-geist-sans)]">
       <ChatContextProvider>
-        <IdBarContextProvider>
-          <Sidebar />
-          <IdBar />
-        </IdBarContextProvider>
+        <ProfileContextProvider>
+          <IdBarContextProvider>
+            <Sidebar />
+            <IdBar />
+          </IdBarContextProvider>
 
-        <div className="h-full flex-grow relative">
-          {/* <Image
+          <div className="h-full flex-grow relative">
+            {/* <Image
             src="/images/chat_window_bg.webp"
             height={4160}
             width={6240}
             alt="Chat Window Background"
             className="object-cover object-center absolute z-10 top-0 left-0 opacity-10 w-full h-full"
           /> */}
-          <ChatWindow />
-        </div>
+            <ChatWindow />
+
+            <Profile />
+          </div>
+        </ProfileContextProvider>
       </ChatContextProvider>
 
       {/* <button onClick={sendMessage}>Send</button> */}
