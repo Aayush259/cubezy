f9e237b884de09aa771f5db4a202da42ae5f9b46
Aayush <aayushk4980@gmail.com>
2024-12-06 23:33:44 +0530

    Created ProfileContext.tsx

---
+++ src/contexts/ProfileContext.tsx
@@ -0,0 +1,97 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { IProfileInfo } from "../interfaces/interfaces";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+
+const ProfileContext = createContext<{
+    isProfileOpen: boolean;
+    setIsProfileOpen: (isOpen: boolean) => void;
+    profileId: string | null;
+    setProfileId: (id: string) => void;
+    isLoading: boolean;
+    openProfile: (id: string) => void;
+    profileInfo: IProfileInfo | null;
+    closeProfile: () => void;
+}>({
+    isProfileOpen: false,
+    setIsProfileOpen: (isOpen: boolean) => { },
+    profileId: null,
+    setProfileId: (id: string) => { },
+    isLoading: true,
+    openProfile: (id: string) => { },
+    profileInfo: null,
+    closeProfile: () => { },
+})
+
+const ProfileContextProvider = ({ children }: { children: React.ReactNode }) => {
+
+    // User state from store.
+    const { user } = useSelector((state: RootState) => state.user);
+
+    const [isProfileOpen, setIsProfileOpen] = useState<boolean>(false);     // Whether the profile is open or not.
+    const [profileId, setProfileId] = useState<string | null>(null);    // The id of the profile to be opened.
+    const [isLoading, setIsLoading] = useState<boolean>(true);      // Whether the profile is loading or not.
+    const [profileInfo, setProfileInfo] = useState<IProfileInfo | null>(null);     // The profile info to be displayed.
+
+    // Function to fetch the profile info.
+    const fetchProfileInfo = async (id: string) => {
+        setIsLoading(true);
+        if (!id) {
+            // Todo: Handle error
+            return
+        };
+
+        // Getting token from local storage.
+        const token = localStorage.getItem("token");
+
+        const res = await fetch(`/api/auth/getProfileInfo?id=${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+        const data = await res.json();
+        if (data.message) {
+            // Todo: Handle error
+            return;
+        }
+        setProfileInfo(data);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        fetchProfileInfo(profileId as string);
+    }, [profileId]);
+
+    useEffect(() => {
+        if (user && profileInfo?._id === user._id) {
+            setProfileInfo({
+                ...profileInfo,
+                dp: user.dp,
+            });
+        }
+    }, [user, profileInfo]);
+
+    // Function to open the profile.
+    const openProfile = (id: string) => {
+        setProfileId(id);
+        setIsProfileOpen(true);
+    };
+
+    // Function to close the profile.
+    const closeProfile = () => {
+        setIsProfileOpen(false);
+    };
+
+    return (
+        <ProfileContext.Provider value={{ isProfileOpen, setIsProfileOpen, profileId, setProfileId, isLoading, openProfile, profileInfo, closeProfile }}>
+            {children}
+        </ProfileContext.Provider>
+    )
+}
+
+const useProfileContext = () => useContext(ProfileContext);
+
+export { ProfileContextProvider, useProfileContext };
+
