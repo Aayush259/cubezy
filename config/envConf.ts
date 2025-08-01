8a4a3c9e47a3bf6965a2369ebe9140ce9f5e5926
Aayush259 <aayushk4980@gmail.com>
2025-08-01 23:28:03 +0530

    Update envConf

--- config/envConf.ts
+++ config/envConf.ts
@@ -6,6 +6,11 @@ const env = {
     CLOUD_NAME: String(process.env.CLOUD_NAME),
     API_KEY: String(process.env.API_KEY),
     API_SECRET: String(process.env.API_SECRET),
+    SMTP_HOST: String(process.env.SMTP_HOST),
+    SMTP_PORT: Number(process.env.SMTP_PORT),
+    SMTP_USER: String(process.env.SMTP_USER),
+    SMTP_PASSWORD: String(process.env.SMTP_PASSWORD),
+    SMTP_FROM: String(process.env.SMTP_FROM),
 }
 
 export default env;
