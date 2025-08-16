32059866dddb866bfe12649368c25100a7fe1e17
Aayush259 <aayushk4980@gmail.com>
2025-08-16 17:28:42 +0530

    Update mailService

--- services/mail/mailService.ts
+++ services/mail/mailService.ts
@@ -6,11 +6,6 @@ export class MailService {
     private transporter: nodemailer.Transporter
 
     constructor() {
-        console.log("env.SMTP_HOST", env.SMTP_HOST)
-        console.log("env.SMTP_PORT", env.SMTP_PORT)
-        console.log("env.SMTP_USER", env.SMTP_USER)
-        console.log("env.SMTP_PASSWORD", env.SMTP_PASSWORD)
-        console.log("env.SMTP_FROM", env.SMTP_FROM)
         this.transporter = nodemailer.createTransport(({
             host: env.SMTP_HOST,
             port: Number(env.SMTP_PORT),
