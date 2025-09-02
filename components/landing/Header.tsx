a54ac4d539ee5b848fc29991aaea9cbc997b109d
aayush259 <aayushk4980@gmail.com>
2025-09-02 23:54:32 +0530

    Completed header

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -5,18 +5,27 @@ import { Button } from "../ui/Button"
 import { useSelector } from "react-redux"
 import { useRouter } from "next/navigation"
 import { RootState } from "@/lib/store/store"
+import { navLinks } from "@/lib/data"
 
 const Header = () => {
     const router = useRouter()
     const { isLoggedIn, user } = useSelector((state: RootState) => state.user)
 
     return (
-        <header className="w-full sticky top-0 left-0 px-3 md:px-10 py-4">
-            <div className="max-w-[2000px] mx-auto flex items-center justify-between gap-4">
+        <header className="w-full sticky top-0 left-0 px-3 md:px-10 py-4 border-b border-gray-800 backdrop-blur-sm">
+            <div className="flex items-center justify-between gap-4">
                 <Link href="/" className="flex items-center gap-2 text-3xl font-semibold system-font">
                     <Image src="/icons/logo.png" alt="Cubezy" width={56} height={56} className="!w-14 !h-auto" />
                     {"Cubezy"}
                 </Link>
+
+                <div className="flex items-center gap-8 text-xl">
+                    {navLinks.map((link, idx) => (
+                        <Link href={link.href} key={idx} className="hover:opacity-80 duration-200">
+                            {link.label}
+                        </Link>
+                    ))}
+                </div>
 
                 {isLoggedIn && user ? (
                     <Button onClick={() => router.push("/chat")}>{"Go to Chat"}</Button>
